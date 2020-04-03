66f5d11522b5049a8e128389ad25dafb588451bf
MrFermz <Mr.Fermz@hotmail.com>
2020-04-03 13:55:53 +0700

    Updates

---
+++ precache-manifest.0e51244352ef75764e8229236135999e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db9f338af01e81c7d8e5c4c1de30e4f",
+    "url": "/rice/index.html"
+  },
+  {
+    "revision": "2894ec1fdba11a3f2866",
+    "url": "/rice/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "26d317b9bb975427c3bb",
+    "url": "/rice/static/js/2.a80541d2.chunk.js"
+  },
+  {
+    "revision": "4de3edfc36f3d156bdd6c2e6dcda6462",
+    "url": "/rice/static/js/2.a80541d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2894ec1fdba11a3f2866",
+    "url": "/rice/static/js/main.107c2ba1.chunk.js"
+  },
+  {
+    "revision": "fe3f3e37f50d40136026",
+    "url": "/rice/static/js/runtime-main.f54a6613.js"
+  }
+]);
